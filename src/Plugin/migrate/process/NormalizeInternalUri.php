8c3ed03a62708e90d9e927d2f3d416664c81b098
Pablo Cerda <pablo@citla.com>
2018-06-06 14:48:08 +0200

    Fixes bugs on URI normalization and removed exception on not existing URI.

--- src/Plugin/migrate/process/NormalizeInternalUri.php
+++ src/Plugin/migrate/process/NormalizeInternalUri.php
@@ -3,7 +3,6 @@
 namespace Drupal\migrate_plugins\Plugin\migrate\process;
 
 use Drupal\Component\Utility\UrlHelper;
-use Drupal\Core\Url;
 use Drupal\migrate\MigrateExecutableInterface;
 use Drupal\migrate\ProcessPluginBase;
 use Drupal\migrate\Row;
@@ -21,24 +20,23 @@ class NormalizeInternalUri extends ProcessPluginBase {
    * {@inheritdoc}
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
-    $path = $value;
+    $path = trim($value);
 
     // Ignore absolute URLS.
-    if (!UrlHelper::isValid($path)) {
+    if (UrlHelper::isValid($path, TRUE)) {
+      return $path;
+    }
+
+    if (empty($path)) {
       return $path;
     }
 
     // Remove leading slash.
-    $path = ltrim('/', $path);
+    $path = preg_replace('#^/+#', '', $path);
 
     // Prepend the internal schema when missing.
-    if (strpos('internal:/', $path) !== FALSE) {
+    if (strpos($path, 'internal:/') === FALSE) {
       $path = 'internal:/' . $path;
-    }
-
-    // If the internal route do not exists throw and exception.
-    if ($url = Url::fromUri($path) && !$url->isRouted()) {
-      throw new MigrateException(sprintf('The path "%s" failed validation.', $path));
     }
 
     return $path;
